0dcdc8dd636d56c0ad3ab698bc85da2804fb21f2
Génesis Mauries♫ <exodo_54@hotmail.com>
2020-11-25 13:07:23 -0600

    Funcion isValid funcional en consola

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import validator from './validator.js';
-
-console.log(validator);
+document.getElementById('btnValid').addEventListener('click',(e)=>{
+    const cardNumber = document.getElementById('cardNumber').value;
+    let validation = validator.isValid(cardNumber);
+    console.log(validation)
+    const textAlert = document.getElementById('textAlert');
+    if(cardNumber === '' || cardNumber.length < 16){
+        e.preventDefault();
+        textAlert.classList.remove('hide');
+        textAlert.innerHTML = 'Ingresa tu número de Tarjeta';
+    }else{
+        //mostrar y ocultar contenedores
+        document.getElementById('containerResult').classList.add('evince');
+        document.getElementById('container').classList.remove('evince');
+        document.getElementById('container').classList.add('hide');
+      
+    }
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,33 @@
 const validator = {
-  // ...
+  isValid:(num)=>{
+    let reverse = '';
+    for(let i= num.length-1; i>=0; i--){
+        reverse = reverse + num[i];
+    }
+    const NewArray = Array.from(reverse).map(Number);
+    let odd = [];
+    for(let i=0; i<NewArray.length; i++){
+        if(i%2!==0){
+            odd.push(NewArray[i]*2);
+        }else{
+            odd.push(NewArray[i]*1);
+        }
+    }
+    let list = [];
+    for(let i=0; i<odd.length; i++){
+        if(odd[i]>=10){
+            list.push((odd[i]-10)+1);
+        }else{
+            list.push(odd[i]*1);
+        }
+    }
+    const total = list.reduce((sum, current) => sum + current, 0);
+    let verified = false;
+    if(total%10===0){
+        verified = true
+    }
+    return verified
+}
 };
 
 export default validator;
